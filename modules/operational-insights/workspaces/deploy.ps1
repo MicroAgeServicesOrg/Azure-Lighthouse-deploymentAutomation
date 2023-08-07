ce8854cc7756761e3e54d72098a8f2970999f76d
David Mejak <dmejak@microageservices.com>
2023-08-07 17:12:07 -0400

    adding deploy for lognalytics

---
+++ modules/operational-insights/workspaces/deploy.ps1
@@ -0,0 +1,67 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$subscriptionId,
+
+    [Parameter(Mandatory=$false)]
+    [string]$deploymentName = 'loganalytics.bicep'
+)
+
+
+$ErrorActionPreference = "Stop"
+
+#Define path to the bicep artifacts (files)
+$bicepFile = ".\modules\operational-insights\workspaces\loganalytics.bicep"
+
+###localTesting - Leave disabled
+#$blueprintPath = "."
+
+
+#Define path to file containing Subscription IDs
+$subscriptionFilePath = ".\subscriptionDeployList_testing.csv"
+
+###localTesting - Leave disabled
+#$subscriptionFilePath = "..\..\subscriptionDeployList.csv"
+
+
+#Read Subscription IDs from CSV file and store in an array
+$subscriptions = Import-Csv $subscriptionFilePath
+
+
+
+
+# If you have a set of subs that never should have deployments
+# But is available to the service principal
+
+#run deployment in each subscription
+if ((!$subscriptionId) -and ($subscriptions)) {
+
+    Write-Output "Subscriptions sepecified in CSV file. Deploying to selected subscriptions" -Verbose
+    foreach ($subscription in $subscriptions) {
+        
+        $subscriptionId = $subscription.subscriptionID
+        Set-AzContext -SubscriptionId $subscriptionId
+
+        New-AzSubscriptionDeployment -Name $deploymentName -TemplateFile $bicepFile
+    }
+}
+
+
+
+
+# using specified subscription during deployment "1 sub deployment"
+else {
+
+    Write-Output "Subscription specified at pipeline. Targeting $subscriptionId" -Verbose
+    Set-AzContext -SubscriptionId $subscriptionId
+
+    Write-Output "Subscriptions sepecified in CSV file. Deploying to selected subscriptions" -Verbose
+    foreach ($subscription in $subscriptions) {
+        
+        $subscriptionId = $subscription.subscriptionID
+        Set-AzContext -SubscriptionId $subscriptionId
+
+        New-AzSubscriptionDeployment -Name "devops_azPolicyDefinition_deploy" -Location "WestUS2" -TemplateFile $bicepFile
+    }
+
+}
--- modules/operational-insights/workspaces/loganalytics.bicep
+++ modules/operational-insights/workspaces/loganalytics.bicep
@@ -1,10 +1,9 @@
 
 param clientcode string
-param location string
-param locationshortcode string
+param location string = 'westus3'
 
 
-var uniquelogworkspacename = '${clientcode}-${locationshortcode}-log-prod'
+var uniquelogworkspacename = '${clientcode}-${location}-log-prod'
 
 resource logAnalyticsWorkspace 'Microsoft.OperationalInsights/workspaces@2022-10-01' = {
     name: uniquelogworkspacename
