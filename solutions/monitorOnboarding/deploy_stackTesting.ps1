697f9d1599293c5d78ef099b0c096f143f46b1bd
David Mejak <dmejak@microageservices.com>
2023-09-27 16:46:23 -0400

    updating test file

--- solutions/monitorOnboarding/deploy_stackTesting.ps1
+++ solutions/monitorOnboarding/deploy_stackTesting.ps1
@@ -11,6 +11,31 @@ param (
 )
 
 
+function TurnOnVMs {
+    # Get all turned off VMs in the subscription
+    $vms = Get-AzVM -Status | Where-Object {$_.PowerState -eq 'VM deallocated'}
+
+    # Define the tag to exclude VMs
+    $excludeTag = "DoNotTurnOn"
+
+    # Turn on each VM in parallel
+    $vms | ForEach-Object -Parallel {
+        $vm = $_
+        # Check if the VM has the exclude tag
+        if ($vm.Tags.ContainsValue($excludeTag)) {
+            Write-Output "Skipping VM $($vm.Name) because it has the $excludeTag tag."
+        }
+        else {
+            # Turn on the VM
+            Write-Output "Turning on VM $($vm.Name)..."
+            Start-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName
+        }
+    }
+}
+# Call the function to execute the code
+
+
+
 
 $ErrorActionPreference = "Stop"
 
@@ -50,6 +75,8 @@ $subscriptions = Import-Csv $subscriptionFilePath
         }
         
         else {
+            TurnOnVMs
+            Start-Sleep -Seconds 30
             New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFile -DenySettingsMode "DenyDelete" -templateParameterObject @{clientCode = $clientCode} -Verbose
         }
 
