58f3e896b4382bea2351a505d613f2c7cd6408da
David Mejak <dmejak@microageservices.com>
2023-09-27 17:33:19 -0400

    adding force

--- solutions/monitorOnboarding/deploy_stackTesting.ps1
+++ solutions/monitorOnboarding/deploy_stackTesting.ps1
@@ -77,7 +77,7 @@ $subscriptions = Import-Csv $subscriptionFilePath
         else {
             TurnOnVMs
             Start-Sleep -Seconds 30
-            New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFile -DenySettingsMode "none" -templateParameterObject @{clientCode = $clientCode} -Verbose
+            New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFile -templateParameterObject @{clientCode = $clientCode} -Verbose -Force -Confirm:$false
         }
 
     }
