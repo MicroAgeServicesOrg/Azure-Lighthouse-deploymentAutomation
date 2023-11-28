dae1969a8cecc9999797956c9fb714f8d8b5341e
David Mejak <dmejak@microageservices.com>
2023-11-28 17:48:13 -0500

    fix -force

--- utilities/helperScripts/multiClientDeployment.ps1
+++ utilities/helperScripts/multiClientDeployment.ps1
@@ -183,7 +183,7 @@ foreach ($subscription in $currentSubscriptions) {
     }
     
     else {
-        New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFilePath -templateParameterObject @{clientCode = $clientCode} -DenySettingsMode "None" -Verbose
+        New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFilePath -templateParameterObject @{clientCode = $clientCode} -DenySettingsMode "None" -Force -Verbose
     }
 
 }
