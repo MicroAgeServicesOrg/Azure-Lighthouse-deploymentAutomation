748f2234ae97829f672c115f5029efaff72fa255
David Mejak <dmejak@microageservices.com>
2023-12-01 17:03:11 -0500

    add additional ElseIf statement to account for both test and no client code switches

--- utilities/helperScripts/multiClientDeployment.ps1
+++ utilities/helperScripts/multiClientDeployment.ps1
@@ -184,6 +184,9 @@ foreach ($subscription in $currentSubscriptions) {
     if ($testDeploy) {
         New-AzSubscriptionDeployment -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFilePath -clientCode $clientCode -WhatIf -Verbose
     }
+    elseif ($testDeploy -and $noClientCode) {
+        New-AzSubscriptionDeployment -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFilePath -WhatIf -Verbose
+    }
     elseif ($noClientCode) {
         New-AzSubscriptionDeploymentStack -Name $deploymentName -Location "WestUS3" -TemplateFile $bicepFilePath -DenySettingsMode "None" -Force -Verbose
     }
